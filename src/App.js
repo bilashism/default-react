b930962b5fea00973c716fd0b13a943bf0324283
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2022-04-04 17:34:53 +0600

    useState & useEffect

--- src/App.js
+++ src/App.js
@@ -1,22 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import "./person.css";
+const personsArr = [
+  { name: "Rubel", job: "Doctor" },
+  { name: "Jon", job: "Farmer" },
+  { name: "Joy", job: "Teacher" }
+];
+function Person(props) {
+  return (
+    <article className="person">
+      <h2>Name: {props.name}</h2>
+      <p>Job: {props.job}</p>
+    </article>
+  );
+}
+function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h2 className="">Count: {count}</h2>
+      <button type="button" onClick={() => setCount(count + 1)}>
+        Increase
+      </button>
+      <button type="button" onClick={() => setCount(count - 1)}>
+        Decrease
+      </button>
+    </div>
+  );
+}
+
+function Users() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(res => res.json())
+      .then(users => setUsers(users))
+      .catch(err => console.log(err));
+  }, []);
+  return (
+    <div className="">
+      <h3 className="">Current Users {users.length}</h3>
+      <ul className="">
+        {users.map((user, i) => (
+          <li key={"user-" + i}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {personsArr.map((p, i) => (
+          <Person name={p.name} job={p.job} key={"k-" + i} />
+        ))}
+        <Counter></Counter>
+        <Users></Users>
       </header>
     </div>
   );
